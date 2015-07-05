c63adf0acbd2ca56e91c97158db792b099589c9d
ccshannon123 <ccshannon123@gmail.com>
2015-07-05 01:14:26 -0600

    Admin user delete confirm

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -3,19 +3,12 @@
 angular.module('jrnyApp')
     .controller('AdminCtrl', function ($scope, $http, Auth, User) {
 
+        $scope.sortType = 'name'; // set the default sort type
+        $scope.sortReverse = false; // set the default sort order
+        $scope.searchUser = ''; // set the default search/filter term
+
         // Use the User $resource to fetch all users
         $scope.users = User.query();
-
-        $scope.delete = function (user) {
-            User.remove({
-                id: user._id
-            });
-            angular.forEach($scope.users, function (u, i) {
-                if (u === user) {
-                    $scope.users.splice(i, 1);
-                }
-            });
-        };
 
         $scope.edit = function (user) {
             User.query({
@@ -24,34 +17,20 @@ angular.module('jrnyApp')
             location.href = "/user-details/" + user._id;
         };
 
-
-        $scope.sortType = 'name'; // set the default sort type
-        $scope.sortReverse = false; // set the default sort order
-        $scope.searchFish = ''; // set the default search/filter term
-
-        // create the list of sushi rolls 
-        $scope.sushi = [
-            {
-                name: 'Cali Roll',
-                fish: 'Crab',
-                tastiness: 2
-            },
-            {
-                name: 'Philly',
-                fish: 'Tuna',
-                tastiness: 4
-            },
-            {
-                name: 'Tiger',
-                fish: 'Eel',
-                tastiness: 7
-            },
-            {
-                name: 'Rainbow',
-                fish: 'Variety',
-                tastiness: 6
+        $scope.confirmDelete = function (user) {
+            var txt;
+            var r = confirm("Are you sure you want to delete this user");
+            if (r == true) {
+                User.remove({
+                    id: user._id
+                });
+                angular.forEach($scope.users, function (u, i) {
+                    if (u === user) {
+                        $scope.users.splice(i, 1);
+                    }
+                });
             }
-  ];
+        }
 
 
     });
